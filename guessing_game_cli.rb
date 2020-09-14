b07d497d7352bf7cee9ff5e7c40072d92868b51a
Elias Delgado <evdelgado99@gmail.com>
2020-09-14 02:15:10 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
   guess = rand(6) + 1
   
   if user_input == guess
-    puts "You guessed the correct number!"
+    return "You guessed the correct number!"
   elsif user_input == "exit"
     puts "Goodbye!"
   else
