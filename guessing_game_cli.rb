90ef1812d55bf1fc25a1e9397c1f4ae82f9eafe9
Elias Delgado <evdelgado99@gmail.com>
2020-09-14 02:02:32 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -11,7 +11,7 @@ def run_guessing_game
   elsif user_input == "exit"
     puts "Goodbye!"
   else
-    puts "Sorry! The computer guessed #{guess}"
+    puts "Sorry! The computer guessed #{guess}."
   end
 
 end
