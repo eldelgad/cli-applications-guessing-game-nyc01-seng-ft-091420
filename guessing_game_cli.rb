8e531cb842519ec62266553f57a650acef3dbdff
Elias Delgado <evdelgado99@gmail.com>
2020-09-14 02:12:20 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,10 +6,9 @@ def run_guessing_game
   user_input = gets.chomp
   
   guess = rand(6) + 1
-  
+  binding.pry
   if user_input == guess
     puts "You guessed the correct number!"
-    binding.pry
   elsif user_input == "exit"
     puts "Goodbye!"
   else
